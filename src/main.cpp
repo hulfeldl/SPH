1ef369fabc382c270024f49312443165ce67cf5c
hulfeldl <hulfeldl@HP-ZBook-15u-G5>
2020-07-28 11:36:05 +0200

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Lorenz
+// Version     :
+// Copyright   : Your copyright notice
+// Description : main file for SPH simulation
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+
+
+
+
+	return 0;
+}
