38f0cb4ea8b90a2c3ecef30b4be6a591faa77269
hulfeldl <hulfeldl@HP-ZBook-15u-G5>
2020-09-24 20:49:33 +0200

    New main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,70 @@
 // Description : main file for SPH simulation
 //============================================================================
 
-#include <iostream>
-using namespace std;
+
+
+#include "Settings/Settings.h"
+#include "SPH/sphSim.h"
+
 
 int main (int argc, char *argv[]) {
+
+	// Create Settings
+	SPH::Settings::Settings s( argc, argv );
+
+	// Create Simulation
+	switch (s.dim()) {
+	case 1: {
+
+		SPH::sphSim<1> Simulation( s );
+
+		// Run, Forest, Run!
+		Simulation.run();
+
+		break;
+	}
+
+	case 2: {
+
+		SPH::sphSim<2> Simulation( s );
+
+		// Run, Forest, Run!
+		Simulation.run();
+
+		break;
+	}
+
+	case 3: {
+
+		SPH::sphSim<3> Simulation( s );
+
+		// Run, Forest, Run!
+		Simulation.run();
+
+		break;
+	}
+
+	default:
+		MYASSERT (false, "Dimension must be 1, 2 or 3!");
+	}
 
 
 
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
